abde287a262923176980bd0fb8a806b72cdcf076
masti777 <mastitesfa444@gmail.com>
2022-07-07 06:17:51 -0700

    second commit

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,9 +5,9 @@ import {FaTelegram} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com/in/masti444" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com/masti777" target="_blank"><FaGithub/></a>
-        <a href="https://t.me/masti_777" target="_blank"><FaTelegram/></a>
+        <a href="https://linkedin.com/in/masti444" ><BsLinkedin/></a>
+        <a href="https://github.com/masti777" ><FaGithub/></a>
+        <a href="https://t.me/masti_777" ><FaTelegram/></a>
 
     </div>
   )
